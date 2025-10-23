b87ca75e712fadc21b2a4ef1290b3926eedde28b
RenyaGit <wadarenya1221@gmail.com>
2025-10-23 21:13:25 +0900

    feat: add package documentation for moqt and remove duplicate comments from mux.go

---
+++ moqt/doc.go
@@ -0,0 +1,3 @@
+// Package moqt implements the MOQ Transfork protocol, providing a multiplexer for track routing.
+// It allows handling of track subscriptions, announcements, and track serving.
+package moqt
